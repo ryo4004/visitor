5c8cb53e4f67c2dafbfe57b987c06f54a6f0a1a9
ryo4004 <kato@ryo4004.net>
2020-09-25 06:46:14 +0900

    共通部分をまとめる

--- client/src/Pages/Pages.js
+++ client/src/Pages/Pages.js
@@ -7,17 +7,34 @@ import Input from './Input/Input'
 import Confirm from './Confirm/Confirm'
 import Exit from './Exit/Exit'
 
+import Logo from '../Component/Logo/Logo'
+
 const Pages = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Base} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/visitor" component={Input} />
-        <Route exact path="/confirm" component={Confirm} />
-        <Route exact path="/exit" component={Exit} />
-      </Switch>
-    </Router>
+    <div className="pages">
+      <div className="logo">
+        <Logo />
+      </div>
+      <div className="layout">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Base} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/visitor" component={Input} />
+            <Route exact path="/confirm" component={Confirm} />
+            <Route exact path="/exit" component={Exit} />
+          </Switch>
+        </Router>
+      </div>
+      <footer>
+        <div>
+          <div className="author">
+            <Logo />
+            <small>&copy; The Wind Ensemble 1985-{new Date().getFullYear()} All Rights Reserved.</small>
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
 
