26c12225202d9af7246dad9cd41e3500bb69714f
ryo4004 <kato@ryo4004.net>
2020-09-26 08:57:42 +0900

    完了画面を修正

--- client/src/Pages/Exit/Exit.js
+++ client/src/Pages/Exit/Exit.js
@@ -1,18 +1,31 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-// import './Exit.css'
+import './Exit.css'
 
 const Exit = () => {
-  const history = useHistory()
   return (
-    <div className="home">
-      <p>ご協力ありがとうございました</p>
-      <div className="button">
-        <button onClick={() => history.push('/home')} onTouchStart={() => {}}>
-          戻る
-        </button>
+    <div className="exit">
+      <div className="title">
+        <h2>Visitor card</h2>
+        <h1>来場者カード</h1>
       </div>
+      <p>ご協力ありがとうございました</p>
+      <div className="link-to-home">
+        <a href="https://winds-n.com" className="link">
+          <div>
+            <div className="text">ホームへ</div>
+            <div className="link-arrow">
+              <i className="fas fa-chevron-right"></i>
+            </div>
+          </div>
+        </a>
+      </div>
+      <p>
+        <Link to="/home" className="back-to-home">
+          もう一度入力する
+        </Link>
+      </p>
     </div>
   )
 }
